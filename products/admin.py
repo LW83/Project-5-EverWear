a82cabbeeb498983e0dada35b8f7af14353e89fd
LW83 <ljbwilkinson@gmail.com>
2023-03-02 16:22:02 +0000

    Add Products app, category and product models and admin

---
+++ products/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from .models import Product, Category
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'size',
+        'colour',
+        'price',
+        'sale',
+        'discounted_price',
+        'in_stock',
+        'stock',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
---
+++ products/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+
+from django.db import models
+
+
+class Category(models.Model):
+    ''' Model for product categories'''
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+COLOUR = ((0, "EverWear Grey"), (1, "Black"), (2, "White"),
+          (3, "EverWear Beige"), (4, "EverWear Green"),
+          (5, "EverWear Navy"), (6, "EverWear Blue"))
+
+SIZE = ((0, "Universal"), (1, "X-Small"), (2, "Small"),
+        (3, "Medium"), (4, "Large"), (5, "X-Large"))
+
+
+class Product(models.Model):
+    '''Model for product information'''
+
+    class Meta:
+        verbose_name_plural = 'Products'
+
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    size = models.IntegerField(choices=SIZE)
+    colour = models.IntegerField(choices=COLOUR)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    """ Stock and sale code leveraged from DNL Bowers """
+    stock = models.IntegerField()
+    in_stock = models.BooleanField(default=True)
+    sale = models.BooleanField(default=False)
+    discounted_price = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=False)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name

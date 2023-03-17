10263b0e5587eb55beb2a264b1da95f3bdf65c2b
LW83 <ljbwilkinson@gmail.com>
2023-03-17 12:20:42 +0000

    Create confirmation email templates and configure webhook handler to send

--- project5/settings.py
+++ project5/settings.py
@@ -53,7 +53,6 @@ INSTALLED_APPS = [
     'checkout',
     'profiles',
     'crispy_forms',
-
 ]
 
 MIDDLEWARE = [
@@ -188,3 +187,4 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'everwear@example.com'

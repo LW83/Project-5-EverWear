ab494191d1691102ab0a89160d845ab03b171273
LW83 <ljbwilkinson@gmail.com>
2023-04-11 12:53:01 +0000

    Update settings for AWS config

--- project5/settings.py
+++ project5/settings.py
@@ -28,8 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = "DEVELOPMENT" in os.environ
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ['project-5.herokuapp.com', 'localhost']
 
@@ -178,6 +177,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'project-5-everwear'
     AWS_S3_REGION_NAME = 'eu-west-1'
